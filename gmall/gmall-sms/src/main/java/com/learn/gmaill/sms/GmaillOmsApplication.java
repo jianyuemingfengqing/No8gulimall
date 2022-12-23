86598b8b8c032301e8575b6f77e5af3194e4fd0a
lin <link.twilght.gu@gmail.com>
2022-12-23 12:59:29 +0800

    后台搭建

---
+++ gmall/gmall-sms/src/main/java/com/learn/gmaill/sms/GmaillOmsApplication.java
@@ -0,0 +1,13 @@
+package com.learn.gmaill.oms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmaillOmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmaillOmsApplication.class, args);
+    }
+
+}
